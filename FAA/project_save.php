b5362df141fbd232445d1830aef2d383fa4b85ec
13A5T0N <braston15@gmail.com>
2020-02-20 09:31:12 -0800

    project save

---
+++ FAA/project_save.php
@@ -0,0 +1,9 @@
+<?
+$naam = $_POST["naam"];
+$beschrijving = $_POST["beschrijving"];
+$budget = $_POST["budget"];
+$start = $_POST["start"];
+$eind = $_POST["eind"];
+$leider = $_POST["leider"];
+
+?>
