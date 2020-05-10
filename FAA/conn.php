f50a62dff39c15c16ec929b7d73c3d0fb7b18d43
13A5T0N <braston15@gmail.com>
2020-05-10 20:20:12 -0300

    removed files

--- FAA/conn.php
+++
@@ -1,7 +0,0 @@
-<?php
-$server = "localhost";
-$password = "";
-$user = "root";
-$db = "projecten";
-$conn = mysqli_connect($server,$user,$password,$db);
-?>
--- FAA/security.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-if (!$_SESSION['username']) {
-    header('location: index.php');
-}
-
-?>
