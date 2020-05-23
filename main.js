3e58eb7efd108587826f08f134cb4d89df92d123
Ginavira <56778116+Ginavira@users.noreply.github.com>
2020-05-23 17:08:29 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -3,3 +3,12 @@ $(document).ready(function() {
 		$('.side-nav').toggleClass('visible');
 	});
 });
+
+function myFunction() {
+  var x = document.getElementById("myInput");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
